c63d365424c7c8d4594ed3c49f07c80c0cc9681e
Dinh Cong <dinhtcpd08605@fpt.edu.vn>
2025-07-30 16:38:11 +0700

    feat:(add SortUtils)

--- src/ss5_polymorphism/employee_manager/Utils/SortUtils.java
+++ src/ss5_polymorphism/employee_manager/Utils/SortUtils.java
@@ -1,4 +1,24 @@
 package ss5_polymorphism.employee_manager.Utils;
 
+import ss5_polymorphism.employee_manager.Employee;
+
+import java.util.List;
+
 public class SortUtils {
+    public static void sortByFor(List<Employee> list, boolean ascending) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                double salaryI = list.get(i).getSalary();
+                double salaryJ = list.get(j).getSalary();
+
+                boolean needSwap = ascending ? salaryI > salaryJ : salaryI < salaryJ;
+
+                if (needSwap) {
+                    Employee temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+    }
 }
