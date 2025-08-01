ca30883372a33b00603668436c87cb53493f33db
Dinh Cong <dinhtcpd08605@fpt.edu.vn>
2025-08-01 21:50:16 +0700

    add CheckInput for input manipulation

--- src/PhoneManage/utils/CheckInput.java
+++ src/PhoneManage/utils/CheckInput.java
@@ -1,4 +1,37 @@
-package PhoneManage.Utils;
+package PhoneManage.utils;
+
+import java.util.Scanner;
 
 public class CheckInput {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String readString() {
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (!input.isEmpty()) {
+                return input;
+            } else {
+                System.out.println("Input cannot be empty. Please try again.");
+            }
+        }
+    }
+
+    public static int readPositiveInteger() {
+        int number;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                if (number > 0) {
+                    scanner.nextLine();
+                    return number;
+                } else {
+                    System.out.println("Please enter a positive integer.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a positive integer.");
+                scanner.next();
+            }
+        }
+    }
 }
