db7576e231eece1c12e6f3fbf56c10a19497f1db
Dinh Cong <dinhtcpd08605@fpt.edu.vn>
2025-08-01 21:53:27 +0700

    add main method to Manger for standalone execution

--- src/PhoneManage/RunPhoneManager.java
+++ src/PhoneManage/RunPhoneManager.java
@@ -1,4 +1,8 @@
 package PhoneManage;
 
 public class RunPhoneManager {
+    public static void main(String[] args) {
+        PhoneManager phoneManager = new PhoneManager();
+        phoneManager.showMenu();
+    }
 }
